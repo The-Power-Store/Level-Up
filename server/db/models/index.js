8e9fd35925e16908f64c3190a303bc43e7b05301
Dani de Waal <danidewaal@aol.com>
2018-02-27 13:10:34 -0500

    wrote model associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,10 @@
-const User = require('./user')
+const User = require('./user');
+const Product = require('./product');
+const Category = require('./category');
+const Review = require('./review');
+const Cart = require('./cart');
+const Order = require('./order');
+
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,12 +13,33 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+Product.belongsTo(Category) //CategoryId on product
+
+Order.belongsTo(User) //UserId on order
+
+Order.belongsToMany(Product, {through: 'ProductsInOrder'}) //ProductsInOrder join table
+Product.belongsToMany(Order, {through: 'ProductsInOrder'})
+
+Cart.belongsTo(User) //cart has UserId
+Cart.hasMany(Product) //cartId on Product
+
+Product.hasMany(Review) //ProductId on review
+Review.belongsTo(User) //UserId on review
+
+Product.hasMany(Category) //ProductId on Category
+Category.hasMany(Product) //CategoryId on product
+
+
+
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+
 module.exports = {
   User
 }

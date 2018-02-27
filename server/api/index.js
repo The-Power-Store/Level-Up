2922ba524d0a50bbca177cffa25b08f3f0a82d4f
michjadams <Mjadams66@gmail.com>
2018-02-27 17:46:42 -0500

    cleans up the modles and implements a seed file

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
-rouer.use('/')
+// router.use('/users', require('./users'))
+// router.use('/')
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')

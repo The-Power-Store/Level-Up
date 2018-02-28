95f578d8b4f54743005dd2c5150a983dff9fa654
Dani de Waal <danidewaal@aol.com>
2018-02-28 15:02:14 -0500

    some products componnent done

--- client/components/products.js
+++ client/components/products.js
@@ -3,15 +3,48 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import store from '../store';
 
+
 class Products extends Component {
   constructor(props) {
     super(props)
-
+    state = {
+      input: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  render() {
-    return (
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      })
+    }
 
+  render() {
+    // const products = this.props.products.filter(product => product.name.match(this.state.input));
+    const products = this.props.products
+    return (
+      <div>
+      <form>
+        <input
+        placeholder="Search for a product"
+        onChange={this.handleChange}
+        />
+      </form>
+      <h1>Products</h1>
+      {
+
+        products.map(product => {
+          return (
+            <div key={product.id}>
+              <Link to={`/products/${product.id}`}>{product.name}</Link>
+              <img src={product.imageUrl}/>
+              <p>{product.description}</p>
+              <p>Price: {product.price}</p>
+            </div>
+          )
+        })
+      }
+      </div>
     )
   }
 }

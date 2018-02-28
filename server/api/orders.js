43507991463afd61294e4cd2c181148b6262173c
Lindsay Maher <lindsaymah3r@gmail.com>
2018-02-28 13:21:02 -0500

    updating routes to consider authenication

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,25 +2,21 @@ const router = require("express").Router();
 const { Order } = require("../db/models");
 module.exports = router;
 
+function createError(status, message) {
+  const error = new Error(message)
+  error.status = status
+  return error
+}
 // all orders for admin to access and look at
 router.get('/', (req, res, next) => {
-  return Order.findAll()
+  if (!req.user) next(createError(401, 'WAIT, you need to log in doofus'))
+  const query = req.user.isAdmin ? {} : { where: { userId: req.user.userId } }
+  return Order.findAll(query)
     .then(orders => res.json(orders))
     .catch(next)
 })
 
-// user wants to view their own history
-router.get('/orderhistory', (req, res, next) => {
-  return Order.findAll({
-    where: {
-      userId: req.user.userId
-    }
-  })
-    .then(orders => res.json(orders))
-    .catch(next)
-})
-
-router.get('/orderhistory/:orderId', (req, res, next) => {
+router.get('/:orderId', (req, res, next) => {
   return Order.findOne({
     where: {
       id: req.params.orderId

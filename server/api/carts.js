0ec446e969a008bf8f62cd8d92c68ae9a083a065
Lindsay Maher <lindsaymah3r@gmail.com>
2018-03-02 12:41:11 -0500

    add heroku stuff

--- server/api/carts.js
+++ server/api/carts.js
@@ -15,7 +15,6 @@ router.get('/:cartId', (req, res, next) => {
 
 // add new cart item
 router.post('/', (req, res, next) => {
-    console.log('U UP?', req.body)
     Cart.create(req.body) //this should be a product and it's information
         .then(createdItem => res.status(201).json(createdItem))
         .catch(next);

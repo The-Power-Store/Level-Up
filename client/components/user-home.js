ca62b3ebc757615724e667fe975c9832c6668af5
Dani de Waal <danidewaal@aol.com>
2018-03-02 17:28:09 -0500

    user login page

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,35 +1,63 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { editUser } from '../store';
 /**
  * COMPONENT
  */
-export const UserHome = (props) => {
-  const {email} = props
+export const UserHome = props => {
+  const { user } = props;
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
-}
+  console.log('user', user)
+  return <div>
+      {user.firstName ? <h3>Welcome back {user.firstName}!</h3> :
+        <div>
+          <h3>Welcome, {user.email}</h3>
+          <h5>Please take a minute to complete your profile!</h5>
+
+          <form onSubmit={event => this.handleSubmit(event, props)}>
+            First Name:
+            <input type="text" name="firstName" />
+            Last Name:
+            <input type="text" name="lastName" />
+            <button type="submit">Update Profile</button>
+          </form>
+        </div>}
+    </div>;
+};
 
 /**
  * CONTAINER
  */
-export const mapState = (state) => {
+const mapStateToProps = state => {
   return {
-    email: state.user.email
-  }
-}
+    user: state.user,
+  };
+};
 
-export default connect(mapState)(UserHome)
+const mapDispatchToProps = (dispatch, ownProps) => {
+   console.log("props", props);
+  return {
+    handleSubmit(event, props) {
+
+      const firstName = event.target.firstName.value;
+      const lastName = event.target.lastName.value;
+
+      event.preventDefault();
+
+      dispatch(editUser(props.user.id, { firstName, lastName }))
+      // .then(() => {
+      //   ownProps.history.push(`/products`)
+      // })
+    }
+  };
+};
+
+export default connect(mapStateToProps)(UserHome);
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
   email: PropTypes.string
-}
-
+};

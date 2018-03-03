5d4ad437596d781beb2153090905b92f7aca615e
dewad2 <danidewaal@aol.com>
2018-03-03 09:33:48 -0500

    Update user-home.js

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {editUser} from '../store';
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {editUser} from '../store'
 /**
  * COMPONENT
  */

869076a0c10d18b4c9337497c362006e09a34924
Daymis Vargas <daymis.vargas@gmail.com>
2018-03-03 17:54:20 -0500

    started tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,6 +1,6 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
+const { expect } = require('chai')
 const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
@@ -8,7 +8,7 @@ const User = db.model('User')
 
 describe('User routes', () => {
   beforeEach(() => {
-    return db.sync({force: true})
+    return db.sync({ force: true })
   })
 
   describe('/api/users/', () => {
@@ -27,6 +27,15 @@ describe('User routes', () => {
         .then(res => {
           expect(res.body).to.be.an('array')
           expect(res.body[0].email).to.be.equal(codysEmail)
+        })
+    })
+
+    it('POST /api/users', () => {
+      return request(app)
+        .post('/api/users')
+        .expect(201)
+        .then(res => {
+
         })
     })
   }) // end describe('/api/users')

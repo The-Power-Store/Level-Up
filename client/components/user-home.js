10bd4b7b2f33a0150ff407b0a864b03d91fc91d3
Dani de Waal <danidewaal@aol.com>
2018-03-02 21:42:49 -0500

    fixed refresh

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,7 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { editUser } from '../store';
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {editUser} from '../store'
+
 /**
  * COMPONENT
  */
@@ -9,32 +10,30 @@ export const UserHome = props => {
   const { user } = props;
 
   return <div>
-      {user.firstName ? <h3>Welcome back {user.firstName}!</h3> :
-        <div>
+      {user.firstName ? <h3>Welcome back {user.firstName}!</h3> : <div>
           <h3>Welcome, {user.email}</h3>
           <h5>Please take a minute to complete your profile!</h5>
-
-          <form onSubmit={(event) => props.handleSubmit(event, props.user.id)}>
-            First Name:
-            <input type="text" name="firstName" />
-            Last Name:
-            <input type="text" name="lastName" />
-            <button type="submit">Update Profile</button>
-          </form>
+            <form onSubmit={event => props.handleSubmit(event, props.user.id)}>
+              First Name:
+              <input type="text" name="firstName" />
+              Last Name:
+              <input type="text" name="lastName" />
+              <button type="submit">Update Profile</button>
+            </form>
         </div>}
     </div>;
-};
+  };
 
 /**
  * CONTAINER
  */
 const mapStateToProps = state => {
   return {
-    user: state.user,
+    user: state.user
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch, ownProps) => {
 
   return {
     handleSubmit(event, id) {
@@ -45,18 +44,9 @@ const mapDispatchToProps = (dispatch) => {
       event.preventDefault();
 
       dispatch(editUser(id, { firstName, lastName }))
-      // .then(() => {
-      //   ownProps.history.push(`/products`)
-      // })
+        window.location.reload();
     }
   };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserHome);
-
-/**
- * PROP TYPES
- */
-// UserHome.propTypes = {
-//   user: PropTypes.object
-// };

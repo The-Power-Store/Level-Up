ce8e960ab828a10346576c75c0434c490c1593b3
Dani de Waal <danidewaal@aol.com>
2018-03-04 17:24:38 -0500

    removed commented out code

--- client/components/user/editProfile.js
+++ client/components/user/editProfile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import { changeAddress, editUser, createAddress, updateUser } from '../../store';
+import { changeAddress, editUser, createAddress } from '../../store';
 
 const EditProfile = props => {
 

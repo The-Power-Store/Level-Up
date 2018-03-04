8ccaae737e1d4370513147e426fcdc3b2b43dc83
michjadams <Mjadams66@gmail.com>
2018-03-04 11:09:44 -0500

    even more fake data

--- seed.js
+++ seed.js
@@ -12,19 +12,47 @@ const {
 
 const productData = {
     product: [{
-        title: "dragon",
+        title: "Snow Dragon",
         description: 'a fierce creature',
         imageUrl: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fimages2.fanpop.com%2Fimage%2Fphotos%2F13900000%2FDragon-Wallpaper-dragons-13975578-1280-800.jpg&f=1",
-        price: 1000,
+        price: 100000,
         stock: 5,
         category: 1, 
     }, {
-        title: "wand",
+        title: "Jerskey Wand",
         description: 'made from a piece of beef jerksey',
         imageUrl: "https://www.printingin3d.eu/pictures/2013/02-voldemort-wand/painted-voldemort-wand.JPG",
         price: 1000,
         stock: 5,
         category: 2, 
+    },{
+        title: "Fhire Dragon",
+        description: 'spicy, like cheetos, but like will kill you',
+        imageUrl: "https://wallpapercave.com/wp/McdCS6c.jpg",
+        price: 1000,
+        stock: 5,
+        category: 2, 
+    },{
+        title: "Healing Potion",
+        description: 'heals everything but paper cuts, stubbed toes and when you bite your tongue',
+        imageUrl: "https://wtbpotions.com/wp-content/uploads/square-green-1.jpg",
+        price: 4600,
+        stock: 34,
+        category: 3, 
+    },{
+        title: "Invisibility Potion",
+        description: '...And they never saw him again',
+        imageUrl: "https://slm-assets2.secondlife.com/assets/6094239/lightbox/Hemlock_Potion_bottle_001.jpg?1345140442",
+        price: 234000,
+        stock: 5,
+        category: 3, 
+    },{
+        title: "Mothers Cooking",
+        description: 'Yes. Youre mother specifically. It is delicious.',
+        imageUrl: "https://slm-assets2.secondlife.com/assets/6094239/lightbox/Hemlock_Potion_bottle_001.jpg?1345140442",
+        price: 234000,
+        stock: 5,
+        category: 3, 
     }]
 }
 
@@ -47,6 +75,12 @@ const userData = {
         isAdmin: true,
         email: 'kdkdkd@gmail.com',
         password: "teeteetee"
+    },{
+        firstName: 'Harry',
+        lastName: 'Otter',
+        isAdmin: false,
+        email: 'theboywholived@gmail.com',
+        password: "iamhotstuff"
     }]
 }
 
@@ -91,6 +125,9 @@ const otherData = {
     }, {
         title: "ice",
         description: "girl, you cold"
+    },{
+        title: "potions",
+        description: "These are all our potions, we know what most of them do"
     }],
     productsInOrder: [{
         quantity: 3,
@@ -103,6 +140,16 @@ const otherData = {
         stars: 2,
         userId: 1,
         productId: 1
+    },{
+        content: "not good jerskey",
+        stars: 1,
+        userId: 3,
+        productId: 2
+    },{
+        content: "gave it to my wife, have not seen her for days",
+        stars: 5,
+        userId: 4,
+        productId: 5
     }],
     cart: [{
         quantity: 7,
@@ -111,7 +158,7 @@ const otherData = {
     },{
         quantity: 3,
         userId: 1,
-        productId: 1
+        productId: 4
     },
     {
         quantity: 12,

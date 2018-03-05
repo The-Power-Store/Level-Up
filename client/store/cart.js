b562f8d786d9dc5bd548d3b9ac6fbdd9375da148
michjadams <Mjadams66@gmail.com>
2018-03-05 15:40:57 -0500

    lol, it werks

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,7 +4,6 @@ const GET_CART = 'GET_CART'
 const GET_GUEST_CART ='GET_GUEST_CART'
 
 const getCart = cart => ({ type: GET_CART, cart })
-const getGuestCart = cart => ({ type: GET_GUEST_CART, cart })
 
 export function fetchCart(userId) {
   return dispatch => {
@@ -17,25 +16,12 @@ export function fetchCart(userId) {
   }
 }
 //potentially need to pass a session id here 
-export function fetchGuestCart(){
-    return dispatch=>{
-        console.log("shouting out from the thunk fetching the GUEST cart data, user ID:")
-        axios.get(`/session/cart/`)
-        .then(res => res.data)
-        .then(cart =>{
-            console.log("the retjfdksjfldksurned item", cart)
-            const action = getGuestCart(cart)
-            dispatch(action)
-        })
-    }
-}
+
 
 export default function wholeCartReducer(state = [], action) {
 
     switch (action.type) {
         case GET_CART:
-            return action.cart
-        case GET_GUEST_CART:
             return action.cart
         default:
             return state

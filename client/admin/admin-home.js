975e2265c0bc02b64a1f0e3d10cd74c77b8c9c25
Dani de Waal <danidewaal@aol.com>
2018-03-05 15:20:46 -0500

    adding admin

--- client/admin/admin-home.js
+++ client/admin/admin-home.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchAllUsers, deleteAccount, editUser } from "../store";
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { fetchAllUsers, deleteAccount, editUser } from "../store"
+import {Route} from 'react-router-dom';
 
 class AdminHome extends Component {
   componentDidMount() {
-    this.props.fetchUserData();
+    this.props.fetchUserData()
   }
 
   render() {
-    console.log("users", this.props.users);
-    return <div>Here</div>;
+    console.log("users", this.props.users)
+    return
+    <div>
+      <h3>What would you like to do?</h3>
+      <Link to="/admin/users" component={UserUpdates}/>
+      <Link to="/admin/products" component={ProductUpdates}/>
+    </div>
   }
 }
 
@@ -17,15 +23,15 @@ const mapStateToProps = state => {
   return {
     users: state.users,
     products: state.products
-  };
-};
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchUserData: () => {
-      dispatch(fetchAllUsers());
+      dispatch(fetchAllUsers())
     }
-  };
-};
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdminHome)

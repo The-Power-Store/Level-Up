7e2f7da727357a22dade805e8fbcddbd694d3e68
Daymis Vargas <daymis.vargas@gmail.com>
2018-03-06 12:56:04 -0500

    changed add update to button

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -51,9 +51,9 @@ class UserHome extends Component {
                 </Link>
               </div>
             ) : (
-              <Link to={`/user/editProfile/${user.id}`}>
-                Add/Update Your Info
-              </Link>
+              <a href={`/user/editProfile/${user.id}`} target="_parent">
+                <button>Add/Update Your Info</button>
+              </a>
             )}
           </div>
 

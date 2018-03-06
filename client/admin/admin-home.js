6e51c3c0f087b93d405db99e527ee916a45108af
Dani de Waal <danidewaal@aol.com>
2018-03-06 13:13:08 -0500

    is admin showing on table

--- client/admin/admin-home.js
+++ client/admin/admin-home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { fetchAllUsers, deleteAccount, editUser } from "../store"
 
 class AdminHome extends Component {
@@ -10,65 +10,67 @@ class AdminHome extends Component {
 
   render() {
     const { users, products, handleSubmit } = this.props
-
     console.log("users", this.props.users)
     return <div className="container">
-      <h2 id="tab-title">USERS</h2>
+        <h2 id="tab-title">USERS</h2>
 
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">USER NAME</th>
-            <th scope="col">EMAIL</th>
-            <th scope="col">UPDATE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            users.map(user => <tr key={user.id}>
-              <th scope="row">
-                {user.firstName} {user.lastName}
-              </th>
-              <td>{user.email}</td>
-              <form onSubmit={(event) => handleSubmit(event, user.id)}>
-                <div>
-                  <td>
-                    <label>Make Admin</label>
-                    <input name="makeAdmin" type="checkbox" />
-                  </td>
-                  <td>
-                    <label>Delete User</label>
-                    <input name="delete" type="checkbox" />
-                  </td>
-                  <td>
-                    <button type="submit">Submit!</button>
-                  </td>
-                </div>
-              </form>
-            </tr>)
-          }
-        </tbody>
-      </table>
-      <br />
-      <br />
-      <div className="admin-product-cont">
-        <h2 className="tab-title">PRODUCTS</h2>
-        <a href="/admin/product/0" target="_parent">
-          <button>Add Product</button>
-        </a>
-        {/* <Link className="add-product-btn" to="/admin/product/0"><h4>Add Product</h4></Link> */}
-        <ul>
-          {products.map(product => <div key={product.id}>
-            <li id="product-list">
-              <Link key={product.id} exact to={`/admin/product/${product.id}`}>
-                {product.title}
-              </Link>
-            </li>
-            <br />
-          </div>)}
-        </ul>
-      </div>
-    </div>;
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">USER NAME</th>
+              <th scope="col">EMAIL</th>
+              <th scope="col">IS ADMIN?</th>
+              <th scope="col">UPDATE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => <tr key={user.id}>
+                <th scope="row">
+                  {user.firstName} {user.lastName}
+                </th>
+                <td>{user.email}</td>
+                {
+                  user.isAdmin ? <td>TRUE</td>
+                  : <td>FALSE</td>
+                }
+                <form onSubmit={event => handleSubmit(event, user.id)}>
+                  <div>
+                    <td>
+                      <label>Make Admin</label>
+                      <input name="makeAdmin" type="checkbox" />
+                    </td>
+                    <td>
+                      <label>Delete User</label>
+                      <input name="delete" type="checkbox" />
+                    </td>
+                    <td>
+                      <button type="submit">Submit!</button>
+                    </td>
+                  </div>
+                </form>
+              </tr>)}
+          </tbody>
+        </table>
+        <br />
+        <br />
+        <div className="admin-product-cont">
+          <h2 className="tab-title">PRODUCTS</h2>
+          <a href="/admin/product/0" target="_parent">
+            <button>Add Product</button>
+          </a>
+          {/* <Link className="add-product-btn" to="/admin/product/0"><h4>Add Product</h4></Link> */}
+          <ul>
+            {products.map(product => <div key={product.id}>
+                <li id="product-list">
+                  <Link key={product.id} exact to={`/admin/product/${product.id}`}>
+                    {product.title}
+                  </Link>
+                </li>
+                <br />
+              </div>)}
+          </ul>
+        </div>
+      </div>;
   }
 }
 
@@ -79,7 +81,9 @@ const mapStateToProps = state => {
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch, ownProps) => {
+  console.log('ownProps', ownProps)
+
   return {
 
     fetchUserData: () => {
@@ -90,9 +94,11 @@ const mapDispatchToProps = dispatch => {
       event.preventDefault();
       if (event.target.makeAdmin) dispatch(editUser(id, { isAdmin: true }))
       else if (event.target.delete) dispatch(deleteAccount(id))
+
+      ownProps.history.push(`/home/${id}`);
     }
 
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AdminHome)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AdminHome))

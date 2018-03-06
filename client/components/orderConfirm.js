981ff4d55d2bb1ed433e9e59da2c873c18f3ec1d
Lindsay Maher <lindsaymah3r@gmail.com>
2018-03-06 16:21:08 -0500

    deletes bundle and some confetti

--- client/components/orderConfirm.js
+++ client/components/orderConfirm.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux'
+// import confetti from 'confetti-js'
 
 // add order number and order email address
 const OrderConfirm = (props) => {
     return (
-        <div className="order_confirmation">
-            <p>Your box of level up goodies is expected to ship on the next business day. You will receive an email when your order ships. Excited?
+        <div>
+            <div className="order_confirmation">
+                {/*<canvas id="my-canvas"></canvas>*/}
+                <p>Your box of level up goodies is expected to ship on the next business day. You will receive an email when your order ships. Excited?
             So are we! xx Mystique Boutique ✨</p>
+            </div>
+
+
         </div>
     )
 }

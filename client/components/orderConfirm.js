12ffed35237867eb243dd5448506e8a2df87697d
Lindsay Maher <lindsaymah3r@gmail.com>
2018-03-07 08:54:37 -0500

    almost finished confetti but not quite

--- client/components/orderConfirm.js
+++ client/components/orderConfirm.js
@@ -4,11 +4,8 @@ import { connect } from 'react-redux'
 
 // add order number and order email address
 const OrderConfirm = (props) => {
-    console.log(window.confetti)
-    // confetti.render();
     return (
         <div>
-
             <div className="order_confirmation">
                 <p>Your box of level up goodies is expected to ship on the next business day. You will receive an email when your order ships. Excited?
             So are we! xx Mystique Boutique ✨</p>

6160336db91dd7aac4e8b2ee365042924cfc3ec7
Lindsay Maher <lindsaymah3r@gmail.com>
2018-03-06 23:45:26 -0500

    testing more confetti things

--- client/components/orderConfirm.js
+++ client/components/orderConfirm.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux'
-// import confetti from 'confetti-js'
+
 
 // add order number and order email address
 const OrderConfirm = (props) => {
+    // console.log(window.confetti)
+    // confetti.render();
     return (
         <div>
+
             <div className="order_confirmation">
-                {/*<canvas id="my-canvas"></canvas>*/}
                 <p>Your box of level up goodies is expected to ship on the next business day. You will receive an email when your order ships. Excited?
             So are we! xx Mystique Boutique ✨</p>
             </div>

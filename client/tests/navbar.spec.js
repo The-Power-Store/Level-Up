a799ef2188de9ee44dce35caceda432d6688bf93
Lindsay Maher <lindsaymah3r@gmail.com>
2018-03-06 20:30:27 -0500

    add category css stuff

---
+++ client/tests/navbar.spec.js
@@ -0,0 +1,9 @@
+import { Navbar, mapState } from './../components'
+import { expect } from 'chai'
+import enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+
+describe('Navbar', () => {
+
+})

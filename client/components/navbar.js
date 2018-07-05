8c3f3ea8002f0066c173940e36ff3a9c38cd0cd9
Dani de Waal <danidewaal@aol.com>
2018-07-05 11:54:02 -0400

    readme

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import store from '../store'
-import { NavLink, withRouter } from 'react-router-dom'
-import { logout, addNewCategory } from '../store'
-import { fetchCart } from '../store/cart'
-import { fetchGuestCart } from '../store/sessionCart'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import store from "../store";
+import { NavLink, withRouter } from "react-router-dom";
+import { logout, addNewCategory } from "../store";
+import { fetchCart } from "../store/cart";
+import { fetchGuestCart } from "../store/sessionCart";
 class Navbar extends Component {
-
   render() {
-    let { handleClick, isLoggedIn, categories, user } = this.props
+    let { handleClick, isLoggedIn, categories, user } = this.props;
 
     return (
       <div>
         <nav>
           <div className="left-navbar">
             <Link to="/products">All Products</Link>
-            {
-              categories.length ? categories.map(category => {
-                return <Link to={`/products/categories/${category.id}`} key={category.id}>{category.title}</Link>
-              }) : null
-            }
+            {categories.length
+              ? categories.map(category => {
+                  return (
+                    <Link
+                      to={`/products/categories/${category.id}`}
+                      key={category.id}
+                    >
+                      {category.title}
+                    </Link>
+                  );
+                })
+              : null}
           </div>
           <div className="logo-navbar">
             <Link to="/">
@@ -34,32 +40,40 @@ class Navbar extends Component {
                 {/* The navbar will show these links after you log in */}
                 <Link to={`/home/${user.id}`}>Home</Link>
                 <Link to="/cart">CART!!</Link>
-                <span className='badge'>{this.props.userCart.map(lineitem => lineitem.quantity)
-                  .reduce((a, b) => { return a + b }, 0)
-                }</span>
+                <span className="badge">
+                  {this.props.userCart
+                    .map(lineitem => lineitem.quantity)
+                    .reduce((a, b) => {
+                      return a + b;
+                    }, 0)}
+                </span>
 
                 <a href="#" onClick={handleClick}>
                   Logout
-          </a>
+                </a>
               </div>
             ) : (
-                <div>
-                  {/* The navbar will show these links before you log in */}
-                  <Link to="/login">Login</Link>
-                  <Link to="/signup">Sign Up</Link>
-                  <Link to="/guestCart">CART!</Link>
-                  <span className='badge'>{
-                    Object.keys(this.props.guestCart).map(key => {
-                      return this.props.guestCart[key]
-                    }).reduce((a, b) => { return a + b }, 0)
-                  }</span>
-                </div>
-              )}
+              <div>
+                {/* The navbar will show these links before you log in */}
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+                <Link to="/guestCart">CART!</Link>
+                <span className="badge">
+                  {Object.keys(this.props.guestCart)
+                    .map(key => {
+                      return this.props.guestCart[key];
+                    })
+                    .reduce((a, b) => {
+                      return a + b;
+                    }, 0)}
+                </span>
+              </div>
+            )}
           </div>
         </nav>
         <hr />
       </div>
-    )
+    );
   }
 }
 
@@ -69,21 +83,21 @@ const mapState = state => {
     categories: state.categories,
     user: state.user,
     userCart: state.cart,
-    guestCart: state.sessionCart
-  }
-}
+    guestCart: state.Cart
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     handleClick() {
-      dispatch(logout())
-      dispatch(fetchCart())
-      dispatch(fetchGuestCart())
+      dispatch(logout());
+      dispatch(fetchCart());
+      dispatch(fetchGuestCart());
     }
-  }
-}
+  };
+};
 
-export default withRouter(connect(mapState, mapDispatch)(Navbar))
+export default withRouter(connect(mapState, mapDispatch)(Navbar));
 
 /**
  * PROP TYPES
@@ -91,4 +105,4 @@ export default withRouter(connect(mapState, mapDispatch)(Navbar))
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,72 +1,79 @@
-const router = require("express").Router()
-const { Order, Cart, Product, ProductsInOrder } = require("../db/models")
-const { isLoggedIn, makeError, isAdmin } = require('./utils')
-module.exports = router
+const router = require('express').Router();
+const { Order, Cart, Product, ProductsInOrder } = require('../db/models');
+const { isLoggedIn, makeError, isAdmin } = require('./utils');
+module.exports = router;
 
 // all orders for admin to access and look at
 router.get('/', isLoggedIn, (req, res, next) => {
-  const query = req.user.isAdmin ? {} : { where: { userId: req.user.id } }
+  const query = req.user.isAdmin ? {} : { where: { userId: req.user.id } };
   return Order.findAll(query)
     .then(orders => res.json(orders))
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.get('/:orderId', (req, res, next) => {
   //add a logged in
   Order.findById(req.params.orderId)
     .then(order => res.json(order))
-    .catch(next)
-})
+    .catch(next);
+});
 
 //create order
 //add a user variable that is a ternary checking if they are admin. if so use req.body.userId
 router.post('/', (req, res, next) => {
   Order.create(req.body)
     .then(order => {
-      const currentOrder = order
-      return currentOrder
+      const currentOrder = order;
+      return currentOrder;
     })
     .then(currentOrder => {
       if (req.user == null) {
         //make the order, like normal
-        Promise.all(Object.keys(req.session.cart).map(key => {
-          return Product.findById(+key)
-            .then(product => {
-              return ProductsInOrder.create({ quantity: req.session.cart[key], price: product.price, orderId: currentOrder.id, productId: product.id })
-            })
-        })
+        Promise.all(
+          Object.keys(req.session.cart).map(key => {
+            return Product.findById(+key).then(product => {
+              return ProductsInOrder.create({
+                quantity: req.session.cart[key],
+                price: product.price,
+                orderId: currentOrder.id,
+                productId: product.id
+              });
+            });
+          })
         )
           .then(data => {
-            req.session.destroy(function (err) {
-            })
+            req.session.destroy(function(err) {});
           })
-          .catch(next)
+          .catch(next);
       } else {
         Cart.findAll({
           where: {
             userId: req.user.id
           }
-        })
-          .then(cartItems => {
-            return Promise.all(cartItems.map(item => {
-              return Product.findById(item.dataValues.productId)
-                .then(product => {
-                  return ProductsInOrder.create({ quantity: item.dataValues.quantity, price: product.price, orderId: currentOrder.id, productId: product.id })
-                })
+        }).then(cartItems => {
+          return Promise.all(
+            cartItems.map(item => {
+              return Product.findById(item.productId).then(product => {
+                return ProductsInOrder.create({
+                  quantity: item.quantity,
+                  price: product.price,
+                  orderId: currentOrder.id,
+                  productId: product.id
+                });
+              });
             })
-            )
-              .then(data => {
-                return Cart.destroyCart(req.user.id)
-              })
-              .then((data) => {
-                res.sendStatus(201)
-              })
-          })
+          )
+            .then(data => {
+              return Cart.destroyCart(req.user.id);
+            })
+            .then(data => {
+              res.sendStatus(201);
+            });
+        });
       }
-    }
-    )
-    .catch(next)
-})
+    })
+    .catch(next);
+});
 
 //can update order status if they are an admin user(using isAdmin util function here)
 router.put('/:orderId', isAdmin, (req, res, next) => {
@@ -76,8 +83,8 @@ router.put('/:orderId', isAdmin, (req, res, next) => {
   })
     //need to make more readable/meaningful
     .then(updates => {
-      const updatedStatus = updates[1][0]
-      res.json(updatedStatus)
+      const updatedStatus = updates[1][0];
+      res.json(updatedStatus);
     })
-    .catch(next)
-})
+    .catch(next);
+});
